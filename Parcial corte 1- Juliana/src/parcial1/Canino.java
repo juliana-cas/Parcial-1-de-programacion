238fc400638f0ba7c5a85f805c70cbdbc2506306
juliana-cas <119370955+juliana-cas@users.noreply.github.com>
2023-03-07 16:05:39 -0500

    Add files via upload

---
+++ Parcial corte 1- Juliana/src/parcial1/Canino.java
@@ -0,0 +1,21 @@
+package parcial1;
+
+abstract public class Canino extends Mamifero{
+
+    protected String color;
+    protected float tamanoColmillos;
+
+    public Canino(float tamanoColmillos, String color, String habitat, float altura, float largo, float peso) {
+        super(habitat, altura, largo, peso);
+        this.color = color;
+        this.tamanoColmillos = tamanoColmillos;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public float getTamanoColmillos() {
+        return tamanoColmillos;
+    }
+}
---
+++ Parcial corte 1- Juliana/src/parcial1/Felino.java
@@ -0,0 +1,21 @@
+package parcial1;
+
+abstract public class Felino extends Mamifero{
+
+    protected float tamanoGarras;
+    protected int velocidad;
+
+    public Felino(float tamanoGarras, int velocidad, String habitat, float altura, float largo, float peso) {
+        super(habitat, altura, largo, peso);
+        this.tamanoGarras = tamanoGarras;
+        this.velocidad = velocidad;
+    }
+
+    public float getTamanoGarras() {
+        return tamanoGarras;
+    }
+
+    public int getVelocidad() {
+        return velocidad;
+    }
+}
